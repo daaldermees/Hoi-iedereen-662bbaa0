58b301e24782d0f36acb26c5787f43197fb4f592
Mees <daaldermees@gmail.com>
2020-07-26 15:07:41 +0200

    tekstmessage 5x herhalen dmv variables/for-loop

--- hello.php
+++ hello.php
@@ -1,4 +1,9 @@
 <?php
-	echo "hoi iedereen \n";
-	echo "nog een keer hoi. op de volgende line?"
+	//echo "hoi iedereen \n";
+	//echo "nog een keer hoi. op de volgende line?";
+
+	$text = "hoi iedereen! \n";
+	for ($i=0;$i<5;$i++){
+		echo $text;
+	};
 ?>
