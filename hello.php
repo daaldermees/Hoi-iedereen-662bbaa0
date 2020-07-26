27c3f5de27b55fb1086f3de763c79f577c3f1405
Mees <daaldermees@gmail.com>
2020-07-26 15:12:39 +0200

    BLAST feedback doorgevoerd

--- hello.php
+++ hello.php
@@ -1,9 +1,8 @@
 <?php
-	//echo "hoi iedereen \n";
-	//echo "nog een keer hoi. op de volgende line?";
-
-	$text = "hoi iedereen! \n";
-	for ($i=0;$i<5;$i++){
-		echo $text;
-	};
+//echo "hoi iedereen \n";
+//echo "nog een keer hoi. op de volgende line?";
+$text = "hoi iedereen! \n";
+for ($i = 0; $i < 5; $i++){
+	echo $text;
+};
 ?>
